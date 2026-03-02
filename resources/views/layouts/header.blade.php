db01be8967e521661217b8b2799ef711b8b35cbb
blueswayne133 <blueswayne133@gmail.com>
2026-03-02 20:33:27 +0700

    updates

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -19,18 +19,19 @@
     <title> Appex Trust Bank- Money Transfer & Banking</title>
     <link rel="icon" type="image/png" href="asset/img/favicon.png">
 
-    <!-- Smartsupp Live Chat script -->
-    <script type="text/javascript">
-        var _smartsupp = _smartsupp || {};
-_smartsupp.key = 'f2127c4af756c8dc66e93297a2e1a92b6b953053';
+<!-- Smartsupp Live Chat script -->
+<script type="text/javascript">
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = '140bbe36cd18d10a1896d7577bf4238ae0456ba6';
 window.smartsupp||(function(d) {
   var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
   s=d.getElementsByTagName('script')[0];c=d.createElement('script');
   c.type='text/javascript';c.charset='utf-8';c.async=true;
   c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
 })(document);
-    </script>
-    <noscript> Powered by <a href=“https://www.smartsupp.com” target=“_blank”>Smartsupp</a></noscript>
+</script>
+<noscript>Powered by <a href="https://www.smartsupp.com" target="_blank">Smartsupp</a></noscript>
+
 </head>
 
 <body>
