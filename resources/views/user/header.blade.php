49ef21147d9d20e43944dba01eb3483865cb49d1
blueswayne133 <blueswayne133@gmail.com>
2026-03-03 06:36:35 +0700

    updates

--- resources/views/user/header.blade.php
+++ resources/views/user/header.blade.php
@@ -145,6 +145,21 @@
             border-color: #049dff;
         }
     </style>
+
+    
+<!-- Smartsupp Live Chat script -->
+<script type="text/javascript">
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = '140bbe36cd18d10a1896d7577bf4238ae0456ba6';
+window.smartsupp||(function(d) {
+  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+  c.type='text/javascript';c.charset='utf-8';c.async=true;
+  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
+</script>
+<noscript>Powered by <a href="https://www.smartsupp.com" target="_blank">Smartsupp</a></noscript>
+
 </head>
 
 <body>
@@ -180,7 +195,7 @@
         </div>
     </div>
 
-    <script type="text/javascript">
+    <!-- <script type="text/javascript">
         (function () {
             var options = {
                 whatsapp: "+17863989017", // WhatsApp number
@@ -192,4 +207,4 @@
             s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
             var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
         })();
-    </script>
+    </script> -->
